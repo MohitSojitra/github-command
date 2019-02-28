660b4499b838283defb693d3a3e62d48457779e4
Mohit Sojitra <mohitsojitra0@gmail.com>
2019-02-28 18:05:41 +0530

    main.c file added

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Even_Odd
+//
+//  Created by Mohit on 15/06/18.
+//  Copyright © 2018 Mohit. All rights reserved.
+//
+
+#include<iostream>
+int main()
+{
+    int n;
+    std::cout<<"enter the nomber"<<std::endl;
+    std::cin>>n;
+    if(n%2==0)
+    {
+        std::cout<<"it is even";
+    }
+    else{
+        std::cout<<"it is odd";
+    }
+    
+    return 0;
+}
